ac25643c671b6842b7a17b6abf2a57ad015170ab
Vishwas Meshram <vishwasmeshram0459@gmail.com>
2021-01-29 00:00:07 +0530

    added Icon component

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
-import AppButton from './app/components/AppButton';
-import AppText from './app/components/AppText';
-import Card from './app/components/Card';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import WelcomeScreen from './app/screens/WelcomeScreen';
+import { Text } from 'react-native';
+import Icon from './app/components/Icon';
+import Screen from './app/components/Screen';
 
 export default function App() {
-	return <MessagesScreen />;
+	return (
+		<Screen>
+			<Icon name='email' size={150} backgroundColor='red' iconColor='white' />
+			{/* <Text>Hi</Text> */}
+		</Screen>
+	);
 }
