c6c62ce410bf93a72838169032e229f1cb74a21e
Vishwas Meshram <vishwasmeshram0459@gmail.com>
2021-02-02 23:58:33 +0530

    added placeholder componenet for imagePicker

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ImageInput from './app/components/ImageInput';
 import ListItem from './app/components/lists/ListItem';
+import Screen from './app/components/Screen';
 import AddListingScreen from './app/screens/AddListingScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
 
 export default function App() {
-	return <AddListingScreen />;
+	const [imageUri, setImageUri] = useState(null);
+	const handleImageChange = uri => {
+		console.log('handleImageChange');
+		setImageUri(uri);
+	};
+	return (
+		<Screen>
+			<ImageInput imageUri={imageUri} onImageChange={handleImageChange} />
+		</Screen>
+	);
 }
--- app/components/ImageInput.jsx
+++ app/components/ImageInput.jsx
@@ -1,56 +1,55 @@
-import React, { useEffect, useState } from 'react';
-import { Alert, Button, Image, StyleSheet } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
+import React, { useEffect } from 'react';
+import { View, StyleSheet, Image, Alert } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { colors } from '../config/colors';
 import Screen from './Screen';
-import AppButton from './AppButton';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import * as ImagePicker from 'expo-image-picker';
 
-function ImageInput(props) {
-	const [imageURI, setImageURI] = useState(null);
-	const getPermission = async () => {
-		const { granted } = await ImagePicker.getMediaLibraryPermissionsAsync();
-		if (!granted) alert('You need to allow this access!');
+function ImageInput({ imageURI }) {
+	const handlePress = () => {
+		if (!imageURI) selectImage();
 	};
-
-	useEffect(() => {
-		getPermission();
-	}, []);
-
-	const onSelectImage = async () => {
-		try {
-			const result = await ImagePicker.launchImageLibraryAsync();
-			// console.log('onSelectImage', result);
-			setImageURI(result.uri);
-		} catch (error) {
-			console.log('Error Occurred', error);
+	const requestPermission = async () => {
+		const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+		if (!granted) {
+			Alert.alert('Delete');
 		}
 	};
+	const selectImage = () => {
+		console.log('selectImage');
+	};
+	useEffect(() => {
+		requestPermission();
+	}, []);
 	return (
-		<Screen style={styles.container}>
-			<Button title='Select Image' color='grey' onPress={onSelectImage} />
-			<TouchableWithoutFeedback
-				onPress={() =>
-					Alert.prompt('Image Selection', 'Do you want to remove image?', [
-						{
-							text: 'Yes',
-							onPress: () => setImageURI(null)
-						},
-						{
-							text: 'Cancel',
-							onPress: () => console.log('Canceled')
-						}
-					])
-				}
-			>
-				{imageURI && <Image source={{ uri: imageURI }} style={{ width: 200, height: 200 }} />}
-			</TouchableWithoutFeedback>
-		</Screen>
+		<TouchableWithoutFeedback onPress={handlePress}>
+			<View style={styles.container}>
+				{!imageURI ? (
+					<MaterialCommunityIcons name='camera' size={40} color={colors.medium} />
+				) : (
+					<Image source={{ uri: imageURI }} style={styles.image} />
+				)}
+			</View>
+		</TouchableWithoutFeedback>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
-		padding: 10
+		backgroundColor: 'red',
+		height: 100,
+		width: 100,
+		justifyContent: 'center',
+		alignItems: 'center',
+		overflow: 'hidden',
+		borderRadius: 15
+	},
+	image: {
+		width: '100%',
+		height: '100%',
+		borderRadius: 15,
+		backgroundColor: colors.dark
 	}
 });
 
